742a5e24c713deb7ccff157d69d04e787b4785e9
Hiro-Alter <brayan.mena@correounivalle.edu.co>
2023-01-28 09:58:51 -0500

    Reto_PR

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-let weatherLondon = require('./weather')
-console.log(weatherLondon.coord.lat)
-console.log(weatherLondon.coord.lon)
+let weatherLondon = require('./weather');
+//console.log(weatherLondon.coord.lat)
+//console.log(weatherLondon.coord.lon)
 
+let pilotAffected = [];
 
-
+for (let pilot of weatherLondon.weather[0].pilotAffected) {
+    if (pilot.age > 18) {
+        pilotAffected.push(pilot);
+    }
+}
+console.log(pilotAffected[0].name);
+console.log(pilotAffected[0].age);
